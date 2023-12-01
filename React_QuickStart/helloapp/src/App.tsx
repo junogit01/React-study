2531a47df4875a4359eebbad300743702a283be7
junogit01 <esens0607@gmail.com>
2023-12-02 00:30:54 +0900

    231202

--- React_QuickStart/helloapp/src/App.tsx
+++ React_QuickStart/helloapp/src/App.tsx
@@ -1,18 +1,19 @@
-import React from "react";
+import { useState } from "react";
 import CountryList from "./CountryList";
-
+import styles from "./styles";
 export type CountryType = {
   no: number;
   country: string;
   visited: boolean;
 };
 const App = () => {
-  const list: Array<CountryType> = [
+  const [msg, setMsg] = useState("World");
+  const [list, setList] = useState<Array<CountryType>>([
     { no: 1, country: "이집트", visited: false },
     { no: 2, country: "일본", visited: true },
     { no: 3, country: "피지", visited: false },
     { no: 4, country: "콜롬비아", visited: false },
-  ];
+  ]);
 
   const addResult = (x: number, y: number) => {
     return (
@@ -23,8 +24,8 @@ const App = () => {
   };
   return (
     <div className="container">
-      <h1>Hello !</h1>
-      <hr className="dash-style" />
+      <h1>Hello {msg}!</h1>
+      <hr style={styles.dashStyle} />
       {addResult(4, 3)}
       <CountryList countries={list} />
     </div>
